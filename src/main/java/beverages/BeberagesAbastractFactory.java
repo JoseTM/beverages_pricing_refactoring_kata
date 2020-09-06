c24fa8d374a09d0495227f391f5f90286cfcbcbd
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2020-09-06 13:59:53 +0100

    Refactorizo la clase abstracta para que tenga un constructor singleton

--- src/main/java/beverages/BeberagesAbastractFactory.java
+++ src/main/java/beverages/BeberagesAbastractFactory.java
@@ -1,7 +1,19 @@
 package beverages;
 
 
+import java.util.Objects;
+
 public abstract class BeberagesAbastractFactory {
+
+    private static Object _beberage;
+
+    protected static Object newInstanceOf(Object beverage) throws IllegalAccessException, InstantiationException {
+        if (Objects.isNull(_beberage)) {
+            _beberage = beverage.getClass().newInstance();
+        }
+        return _beberage;
+    };
+
 
     public abstract Beverage createBeverage();
 
