7cbf89573a944dbb5d18e131b7d5c85016086687
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2020-09-06 19:37:35 +0100

    clase que mantine la copia original de cada elemento y devuelve los clones

--- src/main/java/beverages/Waiter.java
+++ src/main/java/beverages/Waiter.java
@@ -1,17 +1,31 @@
 package beverages;
 
 public class Waiter {
+    private static final Coffee _coffee;
+    private static final Tea _tea;
+    private static final HotChocolate _hotChocolate;
+    private static final TeaWithMilk _teaWithMilk ;
 
-	public static TeaBuilder tea() {
-		return new TeaBuilder();
-	}
-	
-	public static CoffeeBuilder coffee() {
-		return new CoffeeBuilder();
+    static{
+		_coffee = new Coffee();
+		_tea = new Tea();
+		_hotChocolate = new HotChocolate();
+		_teaWithMilk = new TeaWithMilk();
 	}
 
-	public static HotChocolateBuilder hotChocolate() {
-		
-		return new HotChocolateBuilder();
+	public Coffee serveCoffee() {
+		return (Coffee) this._coffee.clone();
+	}
+
+	public Tea serveTea() {
+		return (Tea) this._tea.clone();
+	}
+
+	public Tea serveTeaWithMilk(){
+		return (Tea) this._teaWithMilk.clone();
+	}
+
+	public HotChocolate serveHotChocolate() {
+		return (HotChocolate) this._hotChocolate.clone();
 	}
 }
